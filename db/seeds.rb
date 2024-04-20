e544c15facd8aaafee1e9e9d9afa56f25c90bc40
CawWoo <melodicraven@gmail.com>
2024-04-19 21:07:04 -0500

    Started database, added migrations, seeds, and product controller and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create([
+  { name: 'Product 1', price: 19.99, description: 'This is product 1' },
+  { name: 'Product 2', price: 29.99, description: 'This is product 2' },
+  { name: 'Product 3', price: 39.99, description: 'This is product 3' }
+])
